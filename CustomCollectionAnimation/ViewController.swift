d9bfe783bc6e05e01f4a1680b43f8f99f920de47
Bayram Yelec <bayrammyelec@gmail.com>
2025-01-29 10:19:08 +0300

    Initial Commit

---
+++ CustomCollectionAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCollectionAnimation
+//
+//  Created by Bayram Yeleç on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
